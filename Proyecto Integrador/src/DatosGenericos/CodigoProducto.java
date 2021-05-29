3c89957c29e46fc77a6c40fb07430b306e8097d5
Santiago <fragososantiago34@gmail.com>
2021-05-29 11:40:57 -0500

    Encabezado en clase main, se limpio un poco el formato de los archivos fuente

--- Proyecto Integrador/src/DatosGenericos/CodigoProducto.java
+++ Proyecto Integrador/src/DatosGenericos/CodigoProducto.java
@@ -1,15 +1,7 @@
 package DatosGenericos;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author zebra
- */
+
 public class CodigoProducto {
     
 }
--- Proyecto Integrador/src/PuntoDeVenta.java
+++ Proyecto Integrador/src/PuntoDeVenta.java
@@ -1,18 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*Proyecto Integrador P.O.O.
 
-/**
- *
- * @author zebra
- */
+Autores:
+Gaspar Alonso Cardos Uc
+Jesús Santiago Fragoso Quintal
+Victor Eduardo Mendoza Solís
+Luis Armando Valencia Serrano
+
+Universidad Autónoma de Yucatán - Facultad de Matemáticas
+Licenciatura en Ciencias de la Computación
+
+*/
+
+
 public class PuntoDeVenta {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         // TODO code application logic here
         //Comentario prueba
